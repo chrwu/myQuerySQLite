fa375fc4c2888c34ea8fe93719d5c346811e1799
chrwu <kenjichr@gmail.com>
2015-09-21 21:04:15 -0700

    move display_* function out of class

--- MyQuerySQLite.py
+++ MyQuerySQLite.py
@@ -17,7 +17,7 @@ class MyQuerySQLite:
         self.cursor = self.conn.cursor()
         self.__create_table(self.db_table)
         self.no_of_student = self.get_no_of_student()
-    
+
     def close_db_connection(self):
         self.cursor.close()
         self.conn.close()
@@ -29,26 +29,26 @@ class MyQuerySQLite:
             return result
         except:
             print "create new table failed!"
-    
+
     def generate_data(self, n_data=20, n_subjects=3):
         data = []
         for i in xrange(n_data):
             n_score = ''
-            length = str(hex(4))+str(hex(0))+str(hex(0))+str(hex(0))
+            length = str(hex(4)) + str(hex(0)) + str(hex(0)) + str(hex(0))
             for j in xrange(n_subjects):
                 score = rd.randint(0, 100)
-                tag = str(hex(j+1))
+                tag = str(hex(j + 1))
                 value = str(hex(score))
-                n_score += tag+length+value
-            data.append((i+1, 'a'+str(i+1), rd.choice('MF'), sqlite3.Binary(n_score)))
+                n_score += tag + length + value
+            data.append((i + 1, 'a' + str(i + 1), rd.choice('MF'), sqlite3.Binary(n_score)))
         return data
 
     def insert_data(self, data, flag=False):
         if flag:
             inserted_data = []
             for i in xrange(len(data)):
-                sid = data[i][0]+self.no_of_student
-                name = data[i][1][0]+str(int(data[i][1][1:])+self.no_of_student)
+                sid = data[i][0] + self.no_of_student
+                name = data[i][1][0] + str(int(data[i][1][1:]) + self.no_of_student)
                 inserted_data.append((sid, name, data[i][2], data[i][3]))
             try:
                 self.cursor.executemany('INSERT INTO classA VALUES (?, ?, ?, ?)', inserted_data)
@@ -56,13 +56,6 @@ class MyQuerySQLite:
             except:
                 print 'Insert data failed!'
 
-    # Display students' name by gender (defalut:F&M)
-    def display_student_names_by_gender(self, sex='all'):
-        students = self.get_student_names_by_gender(sex)
-        print "There are %s stuednts" % len(students) + " with gender %s :" % sex
-        for student in self.get_student_names_by_gender(sex):
-            print "Student's name: %s" % student[0]
-        
     def get_student_names_by_gender(self, sex='all'):
         sql = '''SELECT name, gender FROM classA WHERE gender '''
         if sex == 'Female':
@@ -71,15 +64,12 @@ class MyQuerySQLite:
             sql += "= \'M\'"
         else:
             sql += "IS NOT NULL"
-        
+
         try:
             result = self.cursor.execute(sql)
             return [student for student in result]
         except:
             print "Query Students' name failed"
-
-    def display_no_of_student(self):
-        print "There are %s students in classA" % self.no_of_student
 
     # Get #of students
     def get_no_of_student(self):
@@ -96,25 +86,25 @@ class MyQuerySQLite:
         sql = '''SELECT name,score FROM classA'''
         try:
             data = self.cursor.execute(sql)
-            score = [{}]*self.n_subject
+            score = [{}] * self.n_subject
             for x in data:
                 name = x[0]
                 score_data = str(x[1]).split("0x")[1:]
 
                 for i in xrange(self.n_subject):
-                    tag = int(score_data[self.n_tlv*i], 16)
-                    length = int(score_data[self.n_tlv*i+1], 16)
+                    tag = int(score_data[self.n_tlv * i], 16)
+                    length = int(score_data[self.n_tlv * i + 1], 16)
                     if length == 4:
-                        value = int(score_data[self.n_tlv*i+self.n_tlv-1], 16)
+                        value = int(score_data[self.n_tlv * i + self.n_tlv - 1], 16)
                     else:
                         value = 0
-                    category = tag-1
+                    category = tag - 1
                     score[category][name] = value
             return score
         except:
             print "__getScoreData failed!"
 
-    def __get_category(self, subject):
+    def get_category(self, subject):
         subject = subject.lower()
         category = []
         if subject in self.subject_map.values():
@@ -127,33 +117,12 @@ class MyQuerySQLite:
             else:  # all
                 category = [i for i in xrange(4) if i > 0]
         return category
-    
-    # Display the highest Socre People by subject
-    def display_highest_score(self, subject='all'):
-        highest_score = self.get_highest_score(subject)
-        category = self.__get_category(subject)
-        if len(category) == 0:
-            print "There's no subject:", subject
-        else:
-            msg = "The student %s has the highest score %s "
 
-            if subject == 'all':
-                for i in category:
-                    print msg % highest_score[i-1] + " in subject %s" % self.subject_map[i]
-            else:
-                print msg % highest_score[0] + " in subject %s" % subject
-    
     def get_highest_score(self, subject='all'):
-        category = self.__get_category(subject)
+        category = self.get_category(subject)
         score = self.__get_score_data()
-        highest_score = [sorted(score[i-1].items(), key=operator.itemgetter(1))[-1] for i in category]
+        highest_score = [sorted(score[i - 1].items(), key=operator.itemgetter(1))[-1] for i in category]
         return highest_score
-
-    # Display Ranking list
-    def display_ranking_list(self):
-        data = self.get_ranking_list()[::-1]
-        for i in xrange(self.no_of_student):
-            print "rank:", i+1, " name:%s total score:%s" % data[i]
 
     def get_ranking_list(self):
         score = self.__get_score_data()
@@ -166,22 +135,7 @@ class MyQuerySQLite:
         rank = sorted(sum_score.items(), key=operator.itemgetter(1))
         return rank
 
-    # Display Students whose score are above score (default:60)
-    def display_student_above_score(self, subject='all', min_score=60):
-        data = self.get_student_above_score(subject, min_score)
-        category = self.__get_category(subject)
-        if len(category) == 1:
-            print "Score >= %s" % min_score, "of Subject:%s" % self.subject_map[category[0]]
-            for score in data[0]:
-                print "Student: %s score: %s" % score 
-        else:
-            for i in category:
-                print "Score >= %s" % min_score, "of Subject:%s" % self.subject_map[i]
-                for score in data[i-1]:
-                    print "Student: %s score: %s" % score 
-                print "\n"
-            
     def get_student_above_score(self, subject='all', min_score=60):
         data = self.__get_score_data()
-        category = self.__get_category(subject)
-        return [[score for score in data[i-1].items() if score[1] >= min_score] for i in category]
+        category = self.get_category(subject)
+        return [[score for score in data[i - 1].items() if score[1] >= min_score] for i in category]
--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 import MyQuerySQLite as mq
 
+
+# Display students' name by gender (default :F&M)
+def display_student_names_by_gender(self, sex='all'):
+    students = self.get_student_names_by_gender(sex)
+    print "There are %s students" % len(students) + " with gender %s :" % sex
+    for student in self.get_student_names_by_gender(sex):
+        print "Student's name: %s" % student[0]
+
+
+def display_no_of_student(self):
+    print "There are %s students in classA" % self.no_of_student
+
+
+# Display the highest Score People by subject
+def display_highest_score(self, subject='all'):
+    highest_score = self.get_highest_score(subject)
+    category = self.get_category(subject)
+    if len(category) == 0:
+        print "There's no subject:", subject
+    else:
+        msg = "The student %s has the highest score %s "
+
+        if subject == 'all':
+            for i in category:
+                print msg % highest_score[i - 1] + " in subject %s" % self.subject_map[i]
+        else:
+            print msg % highest_score[0] + " in subject %s" % subject
+
+
+# Display Ranking list
+def display_ranking_list(self):
+    data = self.get_ranking_list()[::-1]
+    for i in xrange(self.no_of_student):
+        print "rank:", i + 1, " name:%s total score:%s" % data[i]
+
+
+# Display Students whose score are above score (default:60)
+def display_student_above_score(self, subject='all', min_score=60):
+    data = self.get_student_above_score(subject, min_score)
+    category = self.get_category(subject)
+    if len(category) == 1:
+        print "Score >= %s" % min_score, "of Subject:%s" % self.subject_map[category[0]]
+        for score in data[0]:
+            print "Student: %s score: %s" % score
+    else:
+        for i in category:
+            print "Score >= %s" % min_score, "of Subject:%s" % self.subject_map[i]
+            for score in data[i - 1]:
+                print "Student: %s score: %s" % score
+            print "\n"
+
 if __name__ == '__main__':
     query = mq.MyQuerySQLite('chrwu.db', 'classA')
 
@@ -8,26 +59,26 @@ if __name__ == '__main__':
     query.insert_data(data)
 
     # 1.Display students' name by giving gender (default: all gender)
-    query.display_student_names_by_gender()
-    query.display_student_names_by_gender('Male')
-    query.display_student_names_by_gender('Female')
-    
+    display_student_names_by_gender(query)
+    display_student_names_by_gender(query, 'Male')
+    display_student_names_by_gender(query, 'Female')
+
     # 2. Display # of students in class A
-    query.display_no_of_student()
-    
+    display_no_of_student(query)
+
     # 3. Display the student's name who have the highest score by giving subject
-    query.display_highest_score('chinese')
-    query.display_highest_score('math')
-    query.display_highest_score('english')
-    query.display_highest_score('chemistry')
-    query.display_highest_score('all')
-    
+    display_highest_score(query, 'chinese')
+    display_highest_score(query, 'math')
+    display_highest_score(query, 'english')
+    display_highest_score(query, 'chemistry')
+    display_highest_score(query, 'all')
+
     # 4. Display the ranking list in the class
-    query.display_ranking_list()
-    
+    display_ranking_list(query)
+
     # 5. Display students' name whose score is above giving min. score and subject
     # (default: all subject & min=60)
-    query.display_student_above_score(min_score=70)
-    query.display_student_above_score(subject='math')
+    display_student_above_score(query, min_score=70)
+    display_student_above_score(query, subject='math')
 
     query.close_db_connection()
